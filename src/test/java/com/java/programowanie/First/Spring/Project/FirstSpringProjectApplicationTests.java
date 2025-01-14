f2e31c01f4aaef1656c9ddae74fca8d0262d3737
Sebastian <ciborowski.s@gmail.com>
2025-01-14 01:53:55 +0100

    Spring Project

---
+++ src/test/java/com/java/programowanie/First/Spring/Project/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.programowanie.First.Spring.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
